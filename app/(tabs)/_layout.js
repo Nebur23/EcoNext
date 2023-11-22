57a53d1742f3376d2afc8053b8bd9f9c7b3348fd
Nebur23 <yvesnyemb7@gmail.com>
2023-11-22 21:45:16 +0100

    last modification for hackathon

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,22 +1,25 @@
-import { Tabs } from "expo-router"
-import { AntDesign } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
 
 export default () => {
-    
-    return (
-        <Tabs>
-             <Tabs.Screen 
-                name="home"
-                options={{
-                    tabBarIcon: () => {
-                        return <AntDesign name="home" size={24} color="black" />
-                    }
-                }}
-             />
-              <Tabs.Screen 
-                name="list"
-                
-             />
-        </Tabs>
-    )
-}
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='home'
+        options={{
+          tabBarIcon: () => {
+            return <AntDesign name='home' size={24} color='black' />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name='community'
+        options={{
+          tabBarIcon: () => {
+            return <AntDesign name='wechat' size={24} color='black' />;
+          },
+        }}
+      />
+    </Tabs>
+  );
+};
